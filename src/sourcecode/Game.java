54f4bf92bd66db8c2fcbf25dd6253ca0526ac027
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-08-14 14:24:42 +0700

    finish assignment

--- src/sourcecode/Game.java
+++ src/sourcecode/Game.java
@@ -2,60 +2,47 @@ package sourcecode;
 
 public class Game {
 	public int score() {
-		return scoreForFrame(getCurrentFrame() - 1);
+		return scoreForFrame(itsCurrentFrame);
 	}
 
 	public void add(int pins) {
-		itsThrows[itsCurrentThrow++] = pins;
-		itsScore += pins;
+		itsScorer.addThrow(pins);
 		this.adjustCurrentFrame(pins);
 	}
 
 	private void adjustCurrentFrame(int pins) {
-		if(firstThrowInFrame) {
-			if(pin == 10)
-				itsCurrentFrame++
-			else
-				firstThrowInFrame = false;
-		}else {
-			firstThrowInFrame = true;
-			itsCurrentFrame++;
+		if (lastBallInFrame(pins)) {
+			advanceFrame();
+		} else {
+			firstThrowInFrame = false;
 		}
-		itsCurrentFrame = Math.min(11, itsCurrentFrame);
+	}
+
+	private boolean lastBallInFrame(int pins) {
+		return strike(pins) || (!firstThrowInFrame);
+	}
+
+	private boolean strike(int pins) {
+		return firstThrowInFrame && pins == 10;
+	}
+
+	private boolean adjustFrameForStrike(int pins) {
+		if (pins == 10) {
+			advanceFrame();
+			return true;
+		}
+		return false;
+	}
+
+	private void advanceFrame() {
+		itsCurrentFrame = Math.min(10, itsCurrentFrame + 1);
 	}
 
 	public int scoreForFrame(int theFrame) {
-		ball = 0;
-		int score = 0;
-		for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
-			int firstThrow = itsThrows[ball++];
-			if (firstThrow == 10) {
-				score += 10 + itsThrows[ball] + itsThrows[ball + 1];
-			} else {
-				int secondThrow = itsThrows[ball++];
-
-				int frameScore = firstThrow + secondThrow;
-
-				// spare needs next frames first throw
-				if (frameScore == 10)
-					score += frameScore + itsThrows[ball];
-				else {
-					score += frameScore;
-				}
-			}
-		}
-		return score;
+		return itsScorer.scoreForFrame(theFrame);
 	}
 
-	public int getCurrentFrame() {
-		return itsCurrentFrame;
-	}
-
-	private int ball;
-	private int itsScore = 0;
-	private int[] itsThrows = new int[21];
-	private int itsCurrentThrow = 0;
 	private int itsCurrentFrame = 1;
-	private boolean firstThrow = true;
-
+	private boolean firstThrowInFrame = true;
+	private Scorer itsScorer = new Scorer();
 }
--- src/test/TestGame.java
+++ src/test/TestGame.java
@@ -14,18 +14,10 @@ public class TestGame extends TestCase {
 		g = new Game();
 	}
 
-	// public void testOneThrow() {
-	// g.add(5);
-	// assertEquals(5, g.score());
-	// assertEquals(1, g.getCurrentFrame());
-	// }
-
 	public void testTwoThrowsNoMark() {
 		g.add(5);
 		g.add(4);
 		assertEquals(9, g.score());
-		assertEquals(2, g.getCurrentFrame());
-
 	}
 
 	public void testFourThrowsNoMark() {
@@ -36,8 +28,6 @@ public class TestGame extends TestCase {
 		assertEquals(18, g.score());
 		assertEquals(9, g.scoreForFrame(1));
 		assertEquals(18, g.scoreForFrame(2));
-		assertEquals(3, g.getCurrentFrame());
-
 	}
 
 	public void testSimpleSpare() {
@@ -45,7 +35,6 @@ public class TestGame extends TestCase {
 		g.add(7);
 		g.add(3);
 		assertEquals(13, g.scoreForFrame(1));
-		assertEquals(2, g.getCurrentFrame());
 	}
 
 	public void testSimpleFrameAfterSpare() {
@@ -56,7 +45,6 @@ public class TestGame extends TestCase {
 		assertEquals(13, g.scoreForFrame(1));
 		assertEquals(18, g.scoreForFrame(2));
 		assertEquals(18, g.score());
-		assertEquals(3, g.getCurrentFrame());
 	}
 
 	public void testSimpleStrike() {
@@ -65,7 +53,6 @@ public class TestGame extends TestCase {
 		g.add(6);
 		assertEquals(19, g.scoreForFrame(1));
 		assertEquals(28, g.score());
-		assertEquals(3, g.getCurrentFrame());
 	}
 
 	public void testPerfactGame() {
@@ -73,7 +60,6 @@ public class TestGame extends TestCase {
 			g.add(10);
 		}
 		assertEquals(300, g.score());
-		assertEquals(11, g.getCurrentFrame());
 	}
 
 	public void testEndOfArray() {
